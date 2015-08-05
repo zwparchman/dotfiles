645b93b054f7f0239d0d1425ceb775d050b9a9b9
zz zz <z@z.zz>
2015-08-05 11:07:13 -0400

    newvim update

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash - 
+
+cp -i ./bashrc ~/.bashrc
+cp -i ./inputrc ~/.inputrc
+cp -i ./vimrc ~/.vimrc
+cp -i ./ycm_extra_conf.py ~/.ycm_extra_conf.py
+
+set -o nounset                              # Treat unset variables as an error
+
+
--- newVim.sh
+++ newVim.sh
@@ -3,10 +3,18 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-mkdir newVim
+if [ ! -d newVim ] ; then
+  mkdir newVim
+fi
 cd newVim
 
-hg clone https://code.google.com/p/vim/
+if [ ! -f vim-7.4.tar.bz2 ] ; then
+  wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 
+  tar -xf vim-7.4.tar.bz2
+fi
+
+[ -d vim ] || mv vim74 vim
+
 cd vim
 
 ./configure --with-features=huge \
