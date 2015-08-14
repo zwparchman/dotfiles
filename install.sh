566496ec51609e0827f89eb520097b6dd3fa64f2
zz zz <z@z.zz>
2015-08-14 08:03:39 -0400

    moved a bash flag in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash - 
 
+set -o nounset                              # Treat unset variables as an error
+
 cp -i ./bashrc ~/.bashrc
 cp -i ./inputrc ~/.inputrc
 cp -i ./vimrc ~/.vimrc
 cp -i ./ycm_extra_conf.py ~/.ycm_extra_conf.py
-
-set -o nounset                              # Treat unset variables as an error
-
-
