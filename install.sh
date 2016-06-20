c6712107738eedfd4b504466ba7df94b2a5ee27b
Zack <zwparchman@gmail.com>
2016-06-20 10:19:43 -0400

    added screen rc

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ set -o nounset                              # Treat unset variables as an error
 cp -i ./bashrc ~/.bashrc
 cp -i ./inputrc ~/.inputrc
 cp -i ./vimrc ~/.vimrc
+cp -i ./screenrc ~/.screenrc
 cp -i ./ycm_extra_conf.py ~/.ycm_extra_conf.py
 cp -i bin/smite.py ~/bin/smite
